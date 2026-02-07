ed25863ed63630e3b2ea86c8dade8c12f5f14dbb
UmidYul <yumid253@gmail.com>
2026-02-07 17:13:27 +0500

    ewqeqweqwd

--- server/server.js
+++ server/server.js
@@ -68,6 +68,30 @@ app.use('/api/content', contentRouter);
 
 // Admin auth (UI gate)
 // Page routes (without .html extension)
+// Error pages routes
+app.get('/error', (req, res) => {
+    const code = req.query.code || '500';
+    res.status(parseInt(code)).sendFile(path.join(__dirname, 'views', 'error.html'));
+});
+
+app.get('/maintenance', (req, res) => {
+    res.status(503).sendFile(path.join(__dirname, 'views', 'maintenance.html'));
+});
+
+app.get('/error-test', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'error-test.html'));
+});
+
+// 404 Handler - должен быть после всех роутов
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// Error Handler - обработка серверных ошибок
+app.use((err, req, res, next) => {
+    console.error('Server Error:', err.stack);
+    res.status(err.status || 500).sendFile(path.join(__dirname, 'views', 'error.html'));
+});
 // Frontend pages
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../views/index.html')));
 app.get('/catalog', (req, res) => res.sendFile(path.join(__dirname, '../views/catalog.html')));
